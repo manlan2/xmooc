591195b827fae57f7ff10b52bdb5e29917a439d5
liuhaitao123 <1661814129@qq.com>
2017-05-30 11:54:27 +0800

    delete media file

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,6 +2,19 @@
 
 import xadmin
 from .models import EmailVerifyRecord, Banner
+from xadmin import views
+
+#后台全局配置
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = 'XMOOC'
+    site_footer = '北京牛逼公司'
+    menu_style = 'accordion'
+
 
 class EmailVerifyRecordAdmin(object):
     #展示字段
@@ -20,6 +33,10 @@ class BannerAdmin(object):
     search_fields = ('title', 'index')
     list_filter = ('title', 'url', 'index', 'add_time')
 
-
+#注册models
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+
+#注册BaseSetting
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
