f4f20cecdd2c49d81d5a1c931e52d05d1b79331d
liuhaitao <1661814129@qq.com>
2017-06-01 18:04:47 +0800

    fix

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def courses(request):
+    return HttpResponse('123')
